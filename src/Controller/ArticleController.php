ae8f7d28fc7f34bcc1f5b1b09c6de11a61477ead
Szelbi <reven4on@gmail.com>
2021-11-17 21:57:30 +0100

    twig added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,11 +4,19 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class ArticleController{
+class ArticleController extends AbstractController{
 
+	/**
+	 * @Route("/")
+	 * @Method ({"GET"})
+	 */
 	public function index() {
-		return new Response('<html lang="en"><body>Witam</body></html>');
+//		return new Response('<html lang="en"><body>Witam</body></html>');
+
+		return $this->render('articles/index.html.twig');
 	}
 }
