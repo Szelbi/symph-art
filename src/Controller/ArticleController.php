c64cef67a70991401e2ae28a941c40a31a5fe08b
Szelbi <reven4on@gmail.com>
2021-11-18 21:04:26 +0100

    Showing items from Database

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,26 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-class ArticleController extends AbstractController{
+class ArticleController extends AbstractController
+{
 
 	/**
 	 * @Route("/")
 	 * @Method ({"GET"})
 	 */
-	public function index() {
+	public function index()
+	{
 //		return new Response('<html lang="en"><body>Witam</body></html>');
 
-		$articles = ['First', 'Second', 'Third'];
+		$articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
 		return $this->render('articles/index.html.twig', array(
 			'articles' => $articles
 		));
 
 	}
+
+	/**
+	 * @Route("/article/save")
+	 */
+	public function save()
+	{
+		$entityManager = $this->getDoctrine()->getManager();
+
+		$article = new Article();
+		$article->setTitle('Article One');
+		$article->setBody('Body for article One');
+
+		$entityManager->persist($article);
+
+		$entityManager->flush();
+
+		return new Response('Saves and article with with id of'. $article->getId());
+	}
+
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,52 +6,45 @@ use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
 
-	/**
-	 * @ORM\Column(type="text", length=100)
-	 */
-	private $title;
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
 
-
-	/**
-	 * @ORM\Column(type="text")
-	 */
-	private $body;
-
-
-
-	// Getters & Setters
-    public function getId(): ?int
-    {
+    // Getters & Setters
+    public function getId() {
         return $this->id;
     }
 
     public function getTitle() {
-    	return $this->title;
-    }
-
-    public function getBody() {
-    	return $this->body;
+        return $this->title;
     }
 
     public function setTitle($title) {
-    	$this->title = $title;
+        $this->title = $title;
+    }
+
+    public function getBody() {
+        return $this->body;
     }
 
     public function setBody($body) {
-    	$this->body = $body;
+        $this->body = $body;
     }
-
-
 }
